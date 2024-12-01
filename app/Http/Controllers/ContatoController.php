fc4d7137d84cb1f5bda01d3ca2d85f5c4d3ad356
Matheus <nmmatheus@hotmail.com>
2024-12-01 01:45:12 -0300

    01-12-24 mensagens dos erros de validacao do formulario

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,20 +8,37 @@ use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato(Request $request) {
+    public function contato(Request $request)
+    {
 
         $motivo_contato = MotivoContato::all();
         return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contato' => $motivo_contato]);
     }
-    public function salvar(Request $request){
+    public function salvar(Request $request)
+    {
         // Validação de dados do request
-        $request->validate([
+
+        $regras = [
             'nome' => 'required|min:2|max:100',
             'telefone' => 'required|min:8|max:12',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
-            'mensagem' => 'required'
-        ]);
+            'mensagem' => 'required|min:10',
+        ];
+
+        $feedback = [
+            'nome.min' => 'O campo Nome precisa ter pelo menos 2 caracteres',
+            'nome.max' => 'O campo Nome precisa ter no máximo 100 caracteres',
+            'telefone.min' => 'O campo Telefone precisa ter pelo menos 8 caracteres',
+            'telefone.max' => 'O campo Telefone precisa ter no máximo 12 caracteres',
+            'motivo_contatos_id.required' => 'O Motivo do Contato precisa ser preenchido',
+            'mensagem.min' => 'O campo Mensagem precisa ter pelo menos 10 caracteres',
+
+            'required' => 'O campo :attribute precisa ser preenchido',
+            'email' => 'O Email precisa ser válido',
+        ];
+
+        $request->validate($regras, $feedback);
 
         // $contato = new SiteContato();
         // $contato->nome = $request->input('nome');
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,19 +2,37 @@
 <form action={{ route('site.contato') }} method="post">
     @csrf
     <input name="nome" type="text" placeholder="Nome" value="{{ old('nome') }}" class="{{ $classe }}">
-    <br/>
-    <input name="telefone" type="text" placeholder="Telefone" value="{{ old('telefone') }}" class="{{ $classe }}">
-    <br/>
-    <input name="email" type="text" placeholder="E-mail" value="{{ old('email') }}" class="{{ $classe }}">
-    <br/>
-    <select name="motivo_contatos_id" class="{{ $classe }}">
+    @if ($errors->has('nome'))
+    <div style="color: red; font-size: .9rem;">{{ $errors->first('nome') }}</div>
+    @endif
+
+    <input name="telefone" type="text" placeholder="Telefone" value="{{ old('telefone') }}" class="{{ $classe }}" 
+        style="margin-top: 1rem;"> 
+    @if ($errors->has('telefone'))
+    <div style="color: red; font-size: .9rem;">{{ $errors->first('telefone') }}</div>
+    @endif
+    
+    <input name="email" type="email" placeholder="E-mail" value="{{ old('email') }}" class="{{ $classe }}" 
+        style="margin-top: 1rem;">
+    @if ($errors->has('email'))
+    <div style="color: red; font-size: .9rem;">{{ $errors->first('email') }}</div>
+    @endif
+    
+    <select name="motivo_contatos_id" class="{{ $classe }}" style="margin-top: 1rem;">
         <option value="">Qual o motivo do contato?</option>
         @foreach ($motivo_contato as $motivo)
             <option value="{{$motivo->id}}" {{ old('motivo_contatos_id') == $motivo->id ? 'selected' : '' }}>{{$motivo->motivo_contato}}</option>
         @endforeach
     </select>
-    <br/>
-    <textarea name="mensagem" class="{{ $classe }}">{{ old('mensagem')!= '' ? old('mensagem') : 'Preencha aqui a sua mensagem'}}</textarea>
-    <br/>
-    <button type="submit" class="{{ $classe }}">Enviar</button>
+    @if ($errors->has('motivo_contatos_id'))
+    <div style="color: red; font-size: .9rem;">{{ $errors->first('motivo_contatos_id') }}</div>
+    @endif
+    
+    <textarea name="mensagem" class="{{ $classe }}" style="margin-top: 1rem;"
+        >{{ old('mensagem')!= '' ? old('mensagem') : 'Preencha aqui a sua mensagem'}}</textarea>
+    @if ($errors->has('mensagem'))
+    <div style="color: red; font-size: .9rem;">{{ $errors->first('mensagem') }}</div>
+    @endif
+    
+    <button type="submit" class="{{ $classe }}" style="margin-top: 1rem;">Enviar</button>
 </form>
