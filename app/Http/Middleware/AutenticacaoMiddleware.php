5c98a7a1a626ff3e63066dec04a9a3c03cf631ce
Matheus <nmmatheus@hotmail.com>
2025-01-18 21:33:37 -0300

    18-01-25 Middlewares

---
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Facade\FlareClient\Http\Response;
+
+class AutenticacaoMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (true) {
+            return $next($request); // Próxima requisição
+        } else {
+            return Response('Acesso negado!');
+        } 
+    }
+}
