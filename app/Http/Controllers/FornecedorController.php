0ad9572591e1be2e8a1af335f88240ac07709f34
Matheus <nmmatheus@hotmail.com>
2025-01-26 01:04:08 -0300

    26-01-25

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -6,7 +6,18 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index(){
-        return view('app.fornecedor');
+    public function index()
+    {
+        return view('app.fornecedor.index');
+    }
+
+    public function listar(Request $request)
+    {
+        return view('app.fornecedor.listar');
+    }
+
+    public function adicionar()
+    {
+        return view('app.fornecedor.adicionar');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,14 @@ Route::post('/login', 'LoginController@autenticar')->name('site.login');
 Route::prefix('/app')->middleware('autenticacao')->group(function () {
     Route::get('/home', 'HomeController@index')->name('app.home');
     Route::get('/sair', 'LoginController@sair')->name('app.sair');
+
     Route::get('/cliente', 'ClienteController@index')->name('app.cliente');
+
     Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
+    Route::post('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');
+    Route::get('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
+
+
     Route::get('/produto', 'ProdutoController@index')->name('app.produto');
 });
 
