9ac2f332d4adb8cc9e8f5974d80177af8d4b7eff
Matheus <nmmatheus@hotmail.com>
2025-02-08 16:12:24 -0300

    ci: 08-02-25 Produtos infrxc - order by updated_at desc

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -15,12 +15,10 @@ class ProdutoController extends Controller
      */
     public function index(Request $request)
     {
-        $produtos = Produto::paginate(10);
+        $produtos = Produto::orderBy('updated_at', 'desc')->paginate(10);
 
         return view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all()]);
     }
-
-
 
     /**
      * Show the form for creating a new resource.
